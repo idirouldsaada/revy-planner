5ff281545da173bb65799bf58be9bbcf81cfe104
sarahaa <sarahaa@stud.ntnu.no>
2019-01-28 16:17:06 +0100

    nye sketsjer uke 4

--- src/App.js
+++ src/App.js
@@ -32,57 +32,130 @@ const SKETCHES = [
     actors: [1, 2, 7]
   },
   {
-    name: "PTXD (denne er ikke bra nok)",
-    actors: [1, 3, 6, 8, 11]
-  },
-  {
     name: "Soup Opera",
     actors: [1, 6, 8, 10]
   },
   {
     name: "Tolk",
-    actors: [6, 11]
+    actors: [3, 6, 11]
   },
   {
     name: "Ånd i lampe",
     actors: [2, 4, 5]
   },
   {
-    name: "Konspirasjonsteorier v2",
-    actors: [7, 8, 10, 11]
-  },
-  {
-    name: "Ekstremistisk løsningsorientert optimisme",
+    name: "Ekstremistisk",
     actors: [9]
   },
   {
-    name: "Bodegasafari",
-    actors: [1, 3, 8]
+    name: "Kjærestegranti",
+    actors: [3, 6]
   },
   {
-    name: "Bit for Bit",
-    actors: [3, 4, 6]
+    name: "Hemmelig kult",
+    actors: [2, 3, 5, 9]
   },
   {
-    name: "Et veldig abba semester v3 (svein)",
-    actors: [4, 5, 12]
-  },
-  {
-    name: "Bursdagssang",
-    actors: [1, 3, 6, 8]
-  },
-  {
-    name: "Studentråd",
-    actors: [5]
-  },
-  {
-    name: "Lil Hege",
-    actors: [2]
+    name: "Kresen matanmelder",
+    actors: [4, 2, 7]
   },
   {
     name: "Før i tia",
     actors: [3, 4, 11]
   },
+  {
+    name: "Perkusjonist",
+    actors: [1, 9]
+  },
+  {
+    name: "Forballtrener",
+    actors: [10]
+  },
+  {
+    name: "Daskepott",
+    actors: [4, 6, 7, 8, 10, 12]
+  },
+  {
+    name: "Solskjær",
+    actors: [10, 12]
+  },
+  {
+    name: "Markeringer",
+    actors: [8]
+  },
+  {
+    name: "Help desk",
+    actors: [2, 3, 6, 11]
+  },
+  {
+    name: "Kretsløp",
+    actors: [1, 4]
+  },
+  {
+    name: "Gruppechat",
+    actors: []
+  },
+  {
+    name: "Undertroisk",
+    actors: [6, 9, 11]
+  },
+  {
+    name: "Sippemonolog",
+    actors: []
+  },
+  {
+    name: "Talk slow",
+    actors: [8]
+  },
+  {
+    name: "Omegahistorien",
+    actors: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+  },
+  {
+    name: "Silicon Labs",
+    actors: [1, 8, 9]
+  },
+  {
+    name: "ABBA",
+    actors: [1, 3, 4, 5, 6, 7, 11, 12]
+  },
+  {
+    name: "Studentråd",
+    actors: [11]
+  },
+  {
+    name: "Skapdragvolling",
+    actors: [8]
+  },
+  {
+    name: "OpAmp",
+    actors: [7]
+  },
+  {
+    name: "Lyktestolper",
+    actors: [9, 10]
+  },
+  {
+    name: "Bodegasafari",
+    actors: [1]
+  },
+  {
+    name: "Sugar Daddy",
+    actors: [5]
+  },
+  {
+    name: "Åpning",
+    actors: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+  },
+  {
+    name: "Lille Finale",
+    actors: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+  },
+  {
+    name: "Avslutning",
+    actors: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+  },
+
 ];
 
 class App extends Component {

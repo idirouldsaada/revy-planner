96c9f3aa32b61e5640556f4de7fbb12229af5037
sarahaa <sarahaa@stud.ntnu.no>
2019-01-28 16:24:46 +0100

    uke4 update

--- src/App.js
+++ src/App.js
@@ -144,6 +144,10 @@ const SKETCHES = [
     actors: [5]
   },
   {
+    name: "Lag deg et barn",
+    actors: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+  },
+  {
     name: "Åpning",
     actors: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
   },

a4796329ca8a09b0a34a524a71cab1ed32f3fce5
sarahaa <sarahaa@stud.ntnu.no>
2019-01-29 08:59:35 +0100

    la til sketsjer

--- src/App.js
+++ src/App.js
@@ -159,10 +159,6 @@ const SKETCHES = [
     name: "Avslutning",
     actors: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
   },
-  {
-    name: "Potensiale",
-    actors: []
-  },
 ];
 
 class App extends Component {
